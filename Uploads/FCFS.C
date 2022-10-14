1ff84901a75878c3860166a5d2b0328d24a12cdb
jaycoder26 <115362010+jaycoder26@users.noreply.github.com>
2022-10-14 11:20:59 +0530

    FCFS code in C

---
+++ Uploads/FCFS.C
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+int main()
+
+{
+int process,AT[10],BT[10],CT[10],TAT,WT,i;
+
+printf("Enter the no of process");
+scanf("%d",&process);
+printf("\n");
+
+for (i=0;i<process;i++)
+{
+	
+
+
+printf("Enter the process no %d ",i);
+printf("\n");
+
+printf("Enter the Arrival time ");
+scanf("%d",&AT[i]);
+
+
+printf("Enter the Burst time ");
+scanf("%d",&BT[i]);
+
+}
+for (i=0;i<process;i++)
+{
+	printf("CT:%d",BT[i]);
+	}
+	
+	
+for (
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+return 0;
+
+}
