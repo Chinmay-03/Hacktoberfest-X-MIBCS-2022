4321db0918c1ebce939868feee7d8c5cedd38578
Vishal <vishalc2901@gmail.com>
2022-10-16 17:48:37 +0530

    Added bubble sort code in python

---
+++ Uploads/DSA-main (1)/DSA-main/Sorting Algorithm/bubble_sort.py
@@ -0,0 +1,27 @@
+# Bubble sort in Python
+
+def bubbleSort(array):
+    
+  # loop to access each array element
+  for i in range(len(array)):
+
+    # loop to compare array elements
+    for j in range(0, len(array) - i - 1):
+
+      # compare two adjacent elements
+      # change > to < to sort in descending order
+      if array[j] > array[j + 1]:
+
+        # swapping elements if elements
+        # are not in the intended order
+        temp = array[j]
+        array[j] = array[j+1]
+        array[j+1] = temp
+
+
+a = list(map(int,input("\nEnter the numbers : ").strip().split()))
+
+bubbleSort(a)
+
+print('Sorted Array in Ascending Order:')
+print(a)
