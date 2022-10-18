0272bf44d3b528d40aa2b39afdf2be8849338e0f
atharva <79247758+atharvabhavsar01@users.noreply.github.com>
2022-10-18 19:54:36 +0530

    added bubble sort

---
+++ Uploads/C++ Programs/bubblesort.cpp
@@ -0,0 +1,35 @@
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+ 
+void bubbleSort(int arr[], int n)
+{
+	int i, j;
+	for (i = 0; i < n - 1; i++)
+
+	 
+		for (j = 0; j < n - i - 1; j++)
+			if (arr[j] > arr[j + 1])
+				swap(arr[j], arr[j + 1]);
+}
+ 
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+ 
+int main()
+{
+	int arr[] = { 10,12,2,58,13,6,98};
+	int N = sizeof(arr) / sizeof(arr[0]);
+	bubbleSort(arr, N);
+	cout << "Sorted array: \n";
+	printArray(arr, N);
+	return 0;
+}
+ 
