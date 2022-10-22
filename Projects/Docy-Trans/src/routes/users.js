707b80e5d91d9fe2480d234cf0fc78e18294e547
shivam mishra <shivambmishra10@gmail.com>
2022-10-22 23:45:19 +0530

    file added

--- Projects/Docy-Trans/src/routes/users.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-export const router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
